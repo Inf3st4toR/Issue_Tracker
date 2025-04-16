ae944b803443a9791d1b24a16a6efe92a0315ce9
inf3st4tor <xcharaix@gmail.com>
2025-04-16 04:10:35 +0000

    Completed my GET routes

--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,53 @@
 const { v4: uuidv4 } = require("uuid");
 let arrIssues = [];
 
+//Filter function
+const matchFilters = (issue, filters) => {
+  for (let key in filters) {
+    if (issue[key] != filters[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 module.exports = function (app) {
   app
     .route("/api/issues/:project")
 
+    // GET ROUTE
     .get(function (req, res) {
       let project = req.params.project;
       const arrRes = [];
+      const filters = {};
+
+      if (req.query.issue_title !== undefined) {
+        filters.issue_title = req.query.issue_title;
+      }
+      if (req.query.issue_text !== undefined) {
+        filters.issue_text = req.query.issue_text;
+      }
+      if (req.query.created_by !== undefined) {
+        filters.created_by = req.query.created_by;
+      }
+      if (req.query.assigned_to !== undefined) {
+        filters.assigned_to = req.query.assigned_to;
+      }
+      if (req.query.status_text !== undefined) {
+        filters.status_text = req.query.status_text;
+      }
+      if (req.query.open !== undefined) {
+        filters.open = req.query.open === "true";
+      }
+
       arrIssues.forEach((obj) => {
-        if (obj.project == project) arrRes.push(obj);
+        if (obj.project === project && matchFilters(obj, filters))
+          arrRes.push(obj);
       });
       res.json(arrRes);
     })
 
+    //POST ROUTE
     .post(function (req, res) {
       let project = req.params.project;
       const { issue_title, issue_text, created_by, assigned_to, status_text } =
